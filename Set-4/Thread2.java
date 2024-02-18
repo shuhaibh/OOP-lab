9ca44573767cfb25324926bc21788c02e5e3b63f
Muhammad Shuhaibh <147400263+shuhaibh@users.noreply.github.com>
2024-02-18 21:36:44 +0530

    Create Thread2.java

---
+++ Set-4/Thread2.java
@@ -0,0 +1,59 @@
+//More about Threads
+import java.util.Random;
+
+class Randomthread extends Thread
+{
+        public void run()
+        {
+                Random r = new Random();
+                for(int i=0;i<20;i++)
+                {
+                        int n = r.nextInt(100);
+                        if (n%2==0)
+                        {
+                                new Even(n).start();
+                        }
+                        else
+                        {
+                                new Odd(n).start();
+                        }
+                }
+        }
+}
+
+class Even extends Thread
+{
+        private int num;
+        public Even(int num)
+        {
+                this.num = num;
+        }
+
+        public void run()
+        {
+                System.out.println("Even:-"num*num);
+        }
+}
+
+class Odd extends Thread
+{
+        private int num;
+        public Odd(int num)
+        {
+                this.num=num;
+        }
+        public void run()
+        {
+                System.out.println("Odd:-"num*num*num);
+        }
+}
+
+public class Threadpgm
+{
+        public static void main(String args[])
+        {
+                Randomthread r1 = new Randomthread();
+                r1.start();
+        }
+}
+
